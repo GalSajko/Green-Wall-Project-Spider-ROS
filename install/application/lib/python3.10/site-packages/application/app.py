Solved spider pose calculation bug.

install/application/lib/python3.10/site-packages/application/app.py
@@ -106,7 +106,7 @@ class App(Node):
         if self.is_init:
             # Call service for plant position and watering info.
 
-            goal_pose = np.array([2.0, 0.75, 0.3, 0.0])
+            goal_pose = np.array([3.6, 1.5, 0.3, 0.0])
             spider_pose, _, start_legs_positions = self.json_file_manager.read_spider_state()
 
             walking_instructions_request = custom_interface_helper.prepare_modified_walking_instructions_request((start_legs_positions, goal_pose))
@@ -141,9 +141,8 @@ class App(Node):
         self.__distribute_forces(np.delete(spider.LEGS_IDS, leg_id))
 
         spider_pose = self.__get_spider_pose(spider.LEGS_IDS)
-        print(f"SPIDER POSE BEFORE LEG MOVEMENT: {spider_pose}")
         rpy = spider_pose[3:]
-        pin_to_pin_vector_in_local, leg_base_orientation_in_local = tf.get_pin_to_pin_vector_in_local(leg_id, rpy, pin_to_pin_vector_in_global)
+        pin_to_pin_vector_in_local, leg_base_orientation_in_local = tf.get_global_vector_in_local(leg_id, rpy, pin_to_pin_vector_in_global)
 
         move_leg_request = custom_interface_helper.prepare_move_leg_request((
             leg_id,
@@ -161,6 +160,7 @@ class App(Node):
 
         with self.grippers_states_locker:
             is_attached = self.grippers_attached_states[leg_id]
+            self.get_logger().info(f"IS ATTACHED: {is_attached}")
         if not is_attached:
             self.__automatic_correction(leg_id, leg_base_orientation_in_local, goal_pin_position)
     
@@ -188,9 +188,7 @@ class App(Node):
             [-0.25, 0.0, 0.0],
             [0.25, 0.0, 0.0]
         ])
-        offsets_of_offsets = [0.0, 0.02, -0.02]
         spider_pose = self.__get_spider_pose(np.delete(spider.LEGS_IDS, leg_id))
-        print(f"SPIDER POSE BEFORE CORRECTION: {spider_pose}")
         for offset in offsets:
             move_leg_request = custom_interface_helper.prepare_move_leg_request((
                 leg_id,
@@ -205,7 +203,8 @@ class App(Node):
             ))
             move_leg_response = custom_interface_helper.async_service_call(self.move_leg_client, move_leg_request, self)
 
-            velocity_direction = np.array([global_z_direction_in_local[0] + offset[0], global_z_direction_in_local[1] + offset[1], -global_z_direction_in_local[2]])
+            local_offset = tf.get_global_vector_in_local(leg_id, spider_pose[3:], offset)[0]
+            velocity_direction = np.array([global_z_direction_in_local[0] + local_offset[0], global_z_direction_in_local[1] + local_offset[1], -global_z_direction_in_local[2]], dtype = np.float32)
             move_leg_velocity_mode_request = custom_interface_helper.prepare_move_leg_velocity_mode_request((leg_id, velocity_direction))
             move_leg_velocity_mode_response = custom_interface_helper.async_service_call(self.move_leg_velocity_mode_client, move_leg_velocity_mode_request, self)
 
@@ -214,7 +213,7 @@ class App(Node):
             
             with self.grippers_states_locker:
                 is_attached = self.grippers_attached_states[leg_id]
-            print(f"LEG {leg_id} is attached: {is_attached}")
+            self.get_logger().info(f"IS ATTACHED: {is_attached}")
             if is_attached:
                 return
 

install/calculations/lib/python3.10/site-packages/calculations/kinematics.py
@@ -177,7 +177,7 @@ def platform_forward_kinematics(legs_ids: list, legs_positions_in_global: np.nda
     
     return xyzrpy
 
-def get_spider_pose(legs_ids: list, legs_positions_in_global: np.ndarray, joints_values: np.ndarray) -> np.ndarray:
+def get_spider_pose(legs_ids: list, legs_positions_in_global: np.ndarray, joints_values: np.ndarray, node) -> np.ndarray:
     """Calculate spider's pose in global origin. If more than three legs are given, it calculates spider's pose from each
     combination of these three legs. Finally pose is determined as mean value of all calculations.
 
@@ -192,19 +192,21 @@ def get_spider_pose(legs_ids: list, legs_positions_in_global: np.ndarray, joints
     legs_positions_in_global = np.array(legs_positions_in_global)
     legs_ids = list(legs_ids)
     legs_combinations = list(itt.combinations(legs_ids, 3))
-    poses = np.zeros((len(legs_combinations), 6))
+    poses = np.zeros((len(legs_combinations), 6), dtype = np.float32)
     for combination_index, legs_subset in enumerate(legs_combinations):
         legs_subset = np.array(legs_subset)
         subset_indexes = [legs_ids.index(leg) for leg in legs_subset]
         # Skip calculations if all three selected legs are on the same line.
         if not (np.diff(legs_positions_in_global[subset_indexes][:, 0]).any() and np.diff(legs_positions_in_global[subset_indexes][:, 1]).any()):
+            poses[combination_index] = [np.nan] * 6
             continue
         joints_values_in_legs_subset = joints_values[legs_subset]
         legs_poses = np.zeros([3, 4, 4])
         for idx, leg in enumerate(legs_subset):
             legs_poses[idx] = spider_base_to_leg_tip_forward_kinematics(leg, joints_values_in_legs_subset[idx])
         poses[combination_index] = platform_forward_kinematics(legs_subset, legs_positions_in_global[(subset_indexes)], legs_poses)
-    pose = np.mean(np.array(poses), axis = 0)
+
+    pose = np.nanmean(poses, axis = 0)
 
     return pose
 

install/calculations/lib/python3.10/site-packages/calculations/transformations.py
@@ -54,8 +54,8 @@ def xyzrpy_to_matrix(xyzrpy: list, rotation_only: bool = False) -> np.ndarray:
     
     return rotation_matrix
 
-def get_pin_to_pin_vector_in_local(leg_id: int, rpy: list, pin_to_pin_vector_in_global: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-    """Calculate pin-to-pin vector in leg's local origin.
+def get_global_vector_in_local(leg_id: int, rpy: list, pin_to_pin_vector_in_global: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """Calculate global vector in leg's local origin.
 
     Args:
         leg_id (int): Leg id.
@@ -63,7 +63,7 @@ def get_pin_to_pin_vector_in_local(leg_id: int, rpy: list, pin_to_pin_vector_in_
         pin_to_pin_vecor_in_global (np.ndarray): Vector from one pin to another, given in global origin.
 
     Returns:
-        tuple: 1x3 pin-to-pin vector in leg's local origin and 3x3 orientation matrix of leg's anchor in global origin.
+        tuple: 1x3 vector in leg's local origin and 3x3 orientation matrix of leg's anchor in global origin.
     """
     spider_rotation_in_global = xyzrpy_to_matrix(rpy, True)
     leg_base_orientation_in_global = np.linalg.inv(np.dot(spider_rotation_in_global, spider.T_BASES[leg_id][:3, :3]))