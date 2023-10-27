c0578f2d62ef19aeae6b9234fe1751d356677252
GalSajko <gal.sajko@gmail.com>
2023-10-27 15:27:48 +0200

    Revert last changes.

--- src/gwpspider/controller/controller/joint_velocity_controller.py
+++ src/gwpspider/controller/controller/joint_velocity_controller.py
@@ -22,7 +22,7 @@ from gwpspider_interfaces.msg import LegsStates, DynamixelMotorsData
 from gwpspider_interfaces import gwp_interfaces_data as gid
 
 class JointVelocityController(Node):
-    """Velocity controller class. Some changes.
+    """Velocity controller class.
 
     Args:
         Node (Node): ROS node.
